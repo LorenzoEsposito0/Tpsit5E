e5c6ad0d8989f0b372f6771fb5089fb2540698f0
Lorenzo <121286814+LorenzoEsposito0@users.noreply.github.com>
2024-12-02 23:55:21 +0100

    Manca glossario

---
+++ startbootstrap-scrolling-nav-gh-pages/script.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Inizializza i tooltip di Bootstrap
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
